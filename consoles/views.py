9c0e1d522221447a43aa79aa5c50c5f644a3c53e
KaiXtr <ewertonmatheus2113@gmail.com>
2023-11-07 19:18:02 -0300

    Adicionadas views e templates

--- consoles/views.py
+++ consoles/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Console
 
-# Create your views here.
+def main(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+def consoles(request):
+    nomes = []
+    for i in Console.objects.all().values():
+        nomes.append(i['name'])
+    ctxt = {
+        'nomes': nomes
+    }
+    template = loader.get_template("consoles.html")
+    return HttpResponse(template.render(ctxt,request))
